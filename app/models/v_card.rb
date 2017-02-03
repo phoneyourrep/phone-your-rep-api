df0c86121cde62f37a0f3a619d757961aa457d17
M. Simon Borg <msimonborg@gmail.com>
2017-02-02 22:17:34 -0500

    change migrations

--- app/models/v_card.rb
+++ app/models/v_card.rb
@@ -1,4 +1,4 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 class VCard < ApplicationRecord
   belongs_to :office_location
 end
--- db/migrate/20170203010615_remove_v_card_from_office_location.rb
+++
@@ -1,6 +0,0 @@
-#frozen_string_literal: true
-class RemoveVCardFromOfficeLocation < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :office_locations, :v_card
-  end
-end
