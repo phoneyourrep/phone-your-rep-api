3cf807061c8352efe85b53ada1b0e1a93799a896
M. Simon Borg <msimonborg@gmail.com>
2017-03-21 17:24:45 -0400

    default S3 bucket to phone-your-rep-images if no env var is set

--- app/models/office_location.rb
+++ app/models/office_location.rb
@@ -2,7 +2,9 @@
 class OfficeLocation < ApplicationRecord
   include VCardable
 
-  S3_BUCKET = ENV['PYR_S3_BUCKET']
+  # Set a "PYR_S3_BUCKET" environment variable to your own S3 Bucket
+  # if you want to use your own generated QR Codes.
+  S3_BUCKET = ENV['PYR_S3_BUCKET'] || 'phone-your-rep-images'
 
   belongs_to    :rep, foreign_key: :bioguide_id, primary_key: :bioguide_id
   has_many      :issues
