720025bd32280a331d52b5e0cecb6739e437aa03
M. Simon Borg <msimonborg@gmail.com>
2017-03-21 00:31:04 -0400

    add avatars

---
+++ db/migrate/20170321041123_add_data_column_to_avatars.rb
@@ -0,0 +1,5 @@
+class AddDataColumnToAvatars < ActiveRecord::Migration[5.0]
+  def change
+    add_column :avatars, :data, :bytea
+  end
+end
--- lib/add_photos.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-require_relative '../config/environment.rb'
-
-Rep.all.each { |rep| rep.update(photo: "https://theunitedstates.io/images/congress/450x550/#{rep.bioguide_id}.jpg") }
