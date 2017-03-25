473e22c09540ec265b403fb686a4e537376a421e
M. Simon Borg <msimonborg@gmail.com>
2017-03-25 01:41:24 -0400

    add new zcta raw data file, update import_zcta.rb

--- lib/import_zcta.rb
+++ lib/import_zcta.rb
@@ -15,20 +15,6 @@ class ImportZCTA
 
   private
 
-  def zcta_code(row)
-    zcta5 = row['ZCTA5']
-    case zcta5.size
-    when 4
-      '0' + zcta5
-    when 3
-      '00' + zcta5
-    when 2
-      '000' + zcta5
-    else
-      zcta5
-    end
-  end
-
   def seed_from_csv(file)
     csv_zcta_text = File.read(file)
     csv_zctas     = CSV.parse(csv_zcta_text, headers: true, encoding: 'ISO-8859-1')
@@ -36,23 +22,20 @@ class ImportZCTA
   end
 
   def import_row(row)
-    state_code = row['STATE'].size == 1 ? '0' + row['STATE'] : row['STATE']
-    dis_cod    = row['CD'].size == 1 ? '0' + row['CD'] : row['CD']
-    zcta_code  = zcta_code(row)
-    state_abbr = State.find_by(state_code: state_code).abbr
-    district   = District.find_by(full_code: state_code + dis_cod)
-    zcta       = Zcta.find_or_create_by(zcta: zcta_code)
-    add_zcta_district(district, state_abbr, zcta, zcta_code) unless district.blank?
+    state_abbr = State.find_by(state_code: row['STATE']).abbr
+    district   = District.find_by(full_code: row['STATE'] + row['DISTRICT'])
+    zcta       = Zcta.find_or_create_by(zcta: row['ZCTA'])
+    add_zcta_district(district, state_abbr, zcta, row['ZCTA']) unless district.blank?
   end
 
-  def add_zcta_district(district, state_abbr, zcta, zcta_code)
+  def add_zcta_district(district, state_abbr, zcta, zip_code)
     ZctaDistrict.create(
-      zip_code: zcta_code,
+      zip_code: zip_code,
       district_code:     district.code,
       state:             state_abbr,
       district:          district,
       zcta:              zcta
     )
-    puts "Added district #{district.code} to ZCTA #{zcta_code}"
+    puts "Added district #{district.code} to ZCTA #{zip_code}"
   end
 end
