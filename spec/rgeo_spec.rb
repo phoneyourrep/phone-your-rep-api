283838042035ed2aaa5d1227a3e5ab11c06cb203
M. Simon Borg <msimonborg@gmail.com>
2017-05-07 10:28:42 -0400

    change postgis test

--- spec/rgeo_spec.rb
+++ spec/rgeo_spec.rb
@@ -7,9 +7,5 @@ describe RGeo do
     it 'supports the GEOS library' do
       expect(RGeo::Geos.supported?).to be(true)
     end
-
-    it 'supports Proj4' do
-      expect(RGeo::CoordSys::Proj4.supported?).to be(true)
-    end
   end
 end
