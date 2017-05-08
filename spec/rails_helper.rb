b7fc3dad55030d82c20eaa1ca6f570d62e4ec372
M. Simon Borg <msimonborg@gmail.com>
2017-05-08 10:25:16 -0400

    shuffle around rspec helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,12 +2,23 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 require 'support/factory_girl'
+require 'support/request_helpers'
 require 'rack/test'
 
 # FactoryGirl.find_definitions
@@ -60,4 +71,21 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Clean the database
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  # Request helper methods
+  config.include Requests::JsonHelpers, type: :request
 end
