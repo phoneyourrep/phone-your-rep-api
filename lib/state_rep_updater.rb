699c00d82be407981c46fda0d818c21722b03d5d
M. Simon Borg <msimonborg@gmail.com>
2017-06-24 23:26:01 -0400

    improve state_rep_updater

--- lib/state_rep_updater.rb
+++ lib/state_rep_updater.rb
@@ -33,20 +33,20 @@ class StateRepUpdater
 
   def add_or_update_rep(os_rep, district)
     rep = StateRep.find_or_initialize_by(
-      official_full: os_rep.full_name, district: district, state: state
+      official_id: os_rep.leg_id, district: district, state: state
     )
-    rep.state_leg_id = os_rep.leg_id
-    rep.last         = os_rep.last_name
-    rep.first = os_rep.first_name
-    rep.middle = os_rep.middle_name
-    rep.party = os_rep.party
-    rep.contact_form = os_rep.email
-    rep.active = os_rep.active
-    rep.photo = os_rep.photo_url
-    rep.level = os_rep.level
-    rep.url = os_rep.url
-    rep.chamber = os_rep.chamber
-    rep.suffix = os_rep.suffixes
+    rep.official_full  = os_rep.full_name
+    rep.last           = os_rep.last_name
+    rep.first          = os_rep.first_name
+    rep.middle         = os_rep.middle_name
+    rep.party          = os_rep.party
+    rep.contact_form   = os_rep.email
+    rep.active         = os_rep.active
+    rep.photo          = os_rep.photo_url
+    rep.level          = os_rep.level
+    rep.url            = os_rep.url
+    rep.chamber        = os_rep.chamber
+    rep.suffix         = os_rep.suffixes
     add_or_update_office_locations(rep, os_rep)
     rep.save
   end
@@ -54,10 +54,10 @@ class StateRepUpdater
   def add_or_update_office_locations(rep, os_rep)
     os_rep.offices.each do |os_off|
       off = rep.office_locations.find_or_initialize_by(
-        office_type: os_off.type
+        office_type: os_off.type, rep: rep
       )
-      off.fax     = os_off.fax if os_off.fax
-      off.phone   = os_off.phone if os_off.phone
+      off.fax     = os_off.fax     if os_off.fax
+      off.phone   = os_off.phone   if os_off.phone
       off.address = os_off.address if os_off.address
     end
   end
