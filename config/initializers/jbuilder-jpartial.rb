99a074dfc46408f41bf9de77a4897a306360237f
M. Simon Borg <msimonborg@gmail.com>
2017-07-10 18:04:04 -0400

    update jpartial

--- config/initializers/jbuilder-jpartial.rb
+++ config/initializers/jbuilder-jpartial.rb
@@ -5,4 +5,9 @@ module Jbuilder::Jpartial
     json.self state_url(state.state_code)
     json.extract! state, :state_code, :name, :abbr
   end
+
+  jpartial :_district do |district|
+    json.self district_url(district.full_code)
+    json.extract! district, :full_code, :code, :state_code, :level, :chamber, :name
+  end
 end
