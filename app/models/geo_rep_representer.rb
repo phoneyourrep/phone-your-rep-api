af8e9583dbc203fdd82b2325a9503b8f0d6c4330
M. Simon Borg <msimonborg@gmail.com>
2019-02-02 19:35:36 -0500

    remove reference to Rep.columns from GeoRepRepresenter

--- app/models/geo_rep_representer.rb
+++ app/models/geo_rep_representer.rb
@@ -1,12 +1,49 @@
 # frozen_string_literal: true
 
 class GeoRepRepresenter
-  delegate(*Rep.columns.map(&:name),
-           :state,
-           :district,
-           :sort_offices,
-           :sorted_offices,
-           to: :rep)
+  METHODS = %i[
+    id 
+    district_id
+    state_id
+    role
+    official_full
+    last
+    first
+    middle
+    suffix
+    party
+    contact_form
+    url
+    twitter
+    facebook
+    youtube
+    googleplus
+    committees
+    senate_class
+    bioguide_id
+    photo
+    created_at
+    updated_at
+    nickname
+    instagram
+    instagram_id
+    facebook_id
+    youtube_id
+    twitter_id
+    active
+    type
+    chamber
+    state_leg_id
+    photo_url
+    level
+    official_id
+    state
+    district
+    sort_offices
+    sorted_offices
+  ].freeze
+
+  delegate *METHODS, to: :rep
 
   attr_reader :rep, :coordinates
 
